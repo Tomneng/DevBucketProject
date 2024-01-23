8e05f79f1fbde7d800673ab9604edc846f71f7b9
Tomneng <tomneng12345@gmail.com>
2024-01-23 11:10:00 +0900

    project start

--- src/main/java/com/dbp/controller/common/HomeController.java
+++ src/main/java/com/dbp/controller/common/HomeController.java
@@ -14,6 +14,6 @@ public class HomeController {
     @GetMapping("/home")
     @CrossOrigin
     public ResponseEntity<?> home(){
-        return new ResponseEntity<>("hi", HttpStatus.OK);
+        return new ResponseEntity<>("h3i", HttpStatus.OK);
     }
 }
